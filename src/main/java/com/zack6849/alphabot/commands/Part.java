06ade7270657787649db6d5f1a3521d12722fdac
zack6849 <zcraig29@gmail.com>
2013-07-12 01:52:20 -0400

    Test

--- src/main/java/com/zack6849/alphabot/commands/Part.java
+++ src/main/java/com/zack6849/alphabot/commands/Part.java
@@ -28,9 +28,7 @@ public class Part extends Command
         PircBotX bot = event.getBot();
         if (config.isAdmin(event.getUser().getNick(), event.getUser().getHostmask()))
         {
-            bot.setAutoReconnect(false);
-            bot.quitServer("Bye bye!");
-            bot.shutdown();
+            bot.partChannel(event.getChannel());
         }else{
             bot.sendMessage(event.getChannel(), config.getPermissionDenied().replaceAll("%USERNAME%", event.getUser().getNick()));
         }
