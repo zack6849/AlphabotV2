7c4007755d49b47f676949d9fdc090a1f6e307dc
Zack <Zack@Custom.home>
2013-08-07 05:44:41 -0400

    K, i r make werk.

--- src/main/java/com/zack6849/alphabot/api/Utils.java
+++ src/main/java/com/zack6849/alphabot/api/Utils.java
@@ -4,6 +4,12 @@
  */
 package com.zack6849.alphabot.api;
 
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Response;
+import java.io.IOException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,8 +33,28 @@ public class Utils
             return false;
         }
     }
+
     public static String getTitle(String link)
     {
-        return "";
+        try
+        {
+            AsyncHttpClient client = new AsyncHttpClient();
+            Future<Response> future = client.prepareGet(link).setFollowRedirects(true).execute();
+            Response response = future.get();
+            String output = response.getResponseBody("UTF-8");
+            Pattern p = Pattern.compile("<title>.+</title>");
+            Matcher m;
+            for(String line : output.split("\n")){
+                m = p.matcher(line);
+                if(m.find()){
+                    return line.split("<title>")[1].split("</title>")[0];
+                }
+            }
+            return "No title found???";
+        } catch (Exception ex)
+        {
+            Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 }
