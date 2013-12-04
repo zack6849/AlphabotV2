7ec4be94727d31b36daf59a3648653ffdab99b3a
Zachary Craig <zcraig29@gmail.com>
2013-12-04 05:17:26 -0500

    Testing commit bot.

--- src/main/java/com/zack6849/alphabot/Main.java
+++ src/main/java/com/zack6849/alphabot/Main.java
@@ -4,28 +4,26 @@ import com.zack6849.alphabot.api.Command;
 import com.zack6849.alphabot.api.CommandRegistry;
 import com.zack6849.alphabot.api.Config;
 import com.zack6849.alphabot.api.PermissionManager;
-import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
+import org.pircbotx.exception.IrcException;
 import org.reflections.Reflections;
-import org.slf4j.impl.SimpleLogger;
-
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Hello world!
+ */
 public class Main {
     public static long startup = 0;
-
     public static void main(String[] args) {
-        System.setProperty(SimpleLogger.SHOW_DATE_TIME_KEY, "true");
-        System.setProperty(SimpleLogger.DATE_TIME_FORMAT_KEY, "[HH:mm:ss]");
-        System.setProperty(SimpleLogger.SHOW_THREAD_NAME_KEY, "false");
-        System.setProperty(SimpleLogger.LEVEL_IN_BRACKETS_KEY, "true");
-        System.setProperty(SimpleLogger.SHOW_LOG_NAME_KEY, "false");
-        System.out.println("Starting");
         try {
+            //adding a useless comment to test commit bot.
             startup = System.currentTimeMillis();
+            final PircBotX bot = new PircBotX();
             Config config = new Config();
             PermissionManager manager = new PermissionManager(config);
             System.out.println("Loading and registering commands");
@@ -34,31 +32,39 @@ public class Main {
             Reflections reflections = new Reflections("com.zack6849.alphabot.commands");
             Set<Class<? extends Command>> subTypes = reflections.getSubTypesOf(Command.class);
             for (Class c : subTypes) {
-                Command cmd = CommandRegistry.getCommand(c.getSimpleName());
+                Command cmd = (Command) CommandRegistry.getCommand(c.getSimpleName());
                 System.out.println("Registered command " + cmd.getName() + " as key " + c.getSimpleName());
                 CommandRegistry.register(cmd);
             }
-            //i have no idea what this is, but IDEA wouldn't shut the fuck up about changing it.
-            Configuration.Builder<PircBotX> builder = new Configuration.Builder<PircBotX>();
-            builder.setName(config.getBotNickname());
-            builder.setRealName(config.getBotUsername());
-            builder.setLogin(config.getBotIdent());
-            builder.setFinger(config.getCtcpFinger());
-            builder.setEncoding(Charset.isSupported("UTF-8") ? Charset.forName("UTF-8") : Charset.defaultCharset());
-            builder.setNickservPassword(config.getBotPassword());
-            builder.setVersion("Alphbot v2.0 BETA");
-            builder.setServer(config.getServerHostame(), Integer.parseInt(config.getServerPort()), config.getServerPassword());
-            builder.getListenerManager().addListener(new com.zack6849.alphabot.listeners.MessageEvent(config, manager));
-            builder.getListenerManager().addListener(new com.zack6849.alphabot.listeners.InviteEvent(config, manager));
+            bot.setName(config.getBotNickname());
+            bot.setLogin(config.getBotIdent());
+            bot.getListenerManager().addListener(new com.zack6849.alphabot.listeners.MessageEvent(config, manager));
+            bot.getListenerManager().addListener(new com.zack6849.alphabot.listeners.InviteEvent(config, manager));
+            bot.setVerbose(config.isDebug());
+            bot.setEncoding(Charset.forName("UTF-8"));
+            bot.setAutoReconnectChannels(config.isAutoRejoinChannel());
+            bot.setAutoReconnect(config.isAutoReconnectServer());
+            bot.setAutoNickChange(config.isAutoNickChange());
+            bot.setVersion(config.getCtcpVersion());
+            bot.setFinger(config.getCtcpFinger());
+            bot.setEncoding(Charset.forName("UTF-8"));
+            System.out.println("Done, connecting to irc!");
+            bot.connect(config.getServerHostame(), Integer.parseInt(config.getServerPort()), config.getServerPassword());
+            bot.sendMessage("NickServ", "identify " + config.getBotUsername() + " " + config.getBotPassword());
             for (String channel : config.getChannels()) {
-                builder.addAutoJoinChannel(channel);
+                bot.joinChannel(channel);
             }
-            PircBotX bot = new PircBotX(builder.buildConfiguration());
-            System.out.println("Starting bot...");
-            bot.startBot();
-            System.out.println("Shutting down");
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Scanner in = new Scanner(System.in);
+                    String line = "";
+                    while ((line = in.nextLine()) != null) {
+                        bot.sendRawLineNow(line);
+                    }
+                }
+            }).start();
+        } catch (IOException | IrcException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
